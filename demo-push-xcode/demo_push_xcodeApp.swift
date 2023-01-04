a7f03e07444f5321c8b9fd85185ddc184cf5eb20
Bian Damara <bian.damara@gmail.com>
2023-01-04 21:04:25 +0700

    Initial Commit

---
+++ demo-push-xcode/demo_push_xcodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  demo_push_xcodeApp.swift
+//  demo-push-xcode
+//
+//  Created by Bian Damara on 04/01/23.
+//
+
+import SwiftUI
+
+@main
+struct demo_push_xcodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
